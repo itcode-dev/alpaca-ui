73a982989485b54946025a76e4c3aae3d297a4da
박성진 <psj2716@gmail.com>
2022-07-13 18:31:22 +0900

    :label: [FEATURE] Accordion 컴포넌트 타입 추가
    - Accordion 컴포넌트 타입 추가

---
+++ src/atom/Accordion/Accordion.types.ts
@@ -0,0 +1,21 @@
+/**
+ * Accordion 컴포넌트 타입 모듈
+ *
+ * @author RWB
+ * @since 2022.07.13 Wed 17:35:17
+ */
+
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+
+import { Theme } from '../../common/context';
+
+export type AccordionDirection = 'left' | 'right'
+
+export interface AccordionProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+{
+	title?: string | HTMLElement | null
+	open?: boolean
+	direction?: AccordionDirection
+	transparent?: boolean
+	theme?: Theme
+}
