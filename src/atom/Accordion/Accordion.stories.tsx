47d965e2467dc6d62b372c206d2bd49e6c1601ce
박성진 <psj2716@gmail.com>
2022-07-14 13:58:50 +0900

    :lipstick: [UPDATE] Accordion 컴포넌트 스타일 변경
    - accordion-body 초기값 변경 및 Storybook 예시 추가

--- src/atom/Accordion/Accordion.stories.tsx
+++ src/atom/Accordion/Accordion.stories.tsx
@@ -44,7 +44,8 @@ function getTemplate(args: AccordionProps): ComponentStory<typeof Accordion>
 export const Sandbox = getTemplate.bind({});
 Sandbox.args = {
 	children: (
-		<div>
+		<div style={{ padding: '20px' }}>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
@@ -52,7 +53,141 @@ Sandbox.args = {
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
 		</div>
 	),
+	direction: 'left',
 	round: false,
 	theme: 'light',
-	title: <h3>카테고리</h3>
+	title: <h3>카테고리</h3>,
+	transparent: false
 } as AccordionProps;
+
+/**
+ * 방향 예시 JSX 반환 메서드
+ *
+ * @returns {JSX.Element} JSX
+ */
+export function Direction(): JSX.Element
+{
+	return (
+		<section className={cn('root')}>
+			<div className={cn('row')}>
+				<Accordion title='Direction Left'>
+					<div style={{ padding: '20px' }}>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+					</div>
+				</Accordion>
+			</div>
+
+			<div className={cn('row')}>
+				<Accordion direction='right' title='Direction Right'>
+					<div style={{ padding: '20px' }}>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+					</div>
+				</Accordion>
+			</div>
+		</section>
+	);
+}
+
+/**
+ * 둥근 테두리 예시 JSX 반환 메서드
+ *
+ * @returns {JSX.Element} JSX
+ */
+export function Round(): JSX.Element
+{
+	return (
+		<section>
+			<div className={cn('root')}>
+				<div className={cn('row')}>
+					<Accordion title='Light Round' round>
+						<div style={{ padding: '20px' }}>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						</div>
+					</Accordion>
+				</div>
+			</div>
+
+			<div className={cn('root', 'dark')}>
+				<div className={cn('row')}>
+					<Accordion theme='dark' title='Dark Round' round>
+						<div style={{ padding: '20px' }}>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						</div>
+					</Accordion>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+/**
+ * 테마 예시 JSX 반환 메서드
+ *
+ * @returns {JSX.Element} JSX
+ */
+export function Theme(): JSX.Element
+{
+	return (
+		<section>
+			<div className={cn('root')}>
+				<div className={cn('row')}>
+					<Accordion title='Light Theme'>
+						<div style={{ padding: '20px' }}>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						</div>
+					</Accordion>
+				</div>
+			</div>
+
+			<div className={cn('root', 'dark')}>
+				<div className={cn('row')}>
+					<Accordion theme='dark' title='Dark Theme'>
+						<div style={{ padding: '20px' }}>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						</div>
+					</Accordion>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+/**
+ * 투명 예시 JSX 반환 메서드
+ *
+ * @returns {JSX.Element} JSX
+ */
+export function Transparent(): JSX.Element
+{
+	return (
+		<section>
+			<div className={cn('root')}>
+				<div className={cn('row')}>
+					<Accordion title='Light Transparent' transparent>
+						<div style={{ padding: '10px' }}>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						</div>
+					</Accordion>
+				</div>
+			</div>
+
+			<div className={cn('root', 'dark')}>
+				<div className={cn('row')}>
+					<Accordion theme='dark' title='Dark Transparent' transparent>
+						<div style={{ padding: '10px' }}>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione minus quidem dolorem unde vero, odit, sint itaque quos odio nulla iure possimus blanditiis quam magnam libero natus totam asperiores cupiditate.</p>
+						</div>
+					</Accordion>
+				</div>
+			</div>
+		</section>
+	);
+}
