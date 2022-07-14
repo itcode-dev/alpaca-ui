e190152ddf840bc52aad5a49ba1e9214e15c7747
박성진 <psj2716@gmail.com>
2022-07-14 14:37:43 +0900

    :hammer: [UPDATE] atom 컴포넌트 변경
    - atom 컴포넌트 연결

--- src/atom/index.ts
+++ src/atom/index.ts
@@ -4,3 +4,27 @@
  * @author RWB
  * @since 2022.06.06 Mon 17:47:23
  */
+
+import Accordion from './Accordion';
+import AlpacaProvider from './AlpacaProvider';
+import ArtBox from './ArtBox';
+import Button from './Button';
+import CategoryItem from './CategoryItem';
+import Fab from './Fab';
+import Header from './Header';
+import Input from './Input';
+import Screener from './Screener';
+import Sidebar from './Sidebar';
+
+export default {
+	Accordion,
+	AlpacaProvider,
+	ArtBox,
+	Button,
+	CategoryItem,
+	Fab,
+	Header,
+	Input,
+	Screener,
+	Sidebar
+};
