0cbfc2e961794c8d66187f4f1e7d3c34d6c816a9
RWB0104 <psj2716@gmail.com>
2022-07-20 00:58:28 +0900

    :hammer: [UPDATE] Category 컴포넌트 갱신
    - export 구문 추가

---
+++ src/molecule/Category/index.ts
@@ -0,0 +1,11 @@
+/**
+ * 카테고리 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2022.07.19 Tue 23:25:55
+ */
+
+import Category from './Category';
+
+export default Category;
+export * from './Category.types';
