0e661e80ee2135f26fee138730ebdd1792d79570
RWB0104 <psj2716@gmail.com>
2022-08-01 00:09:48 +0900

    :label: [UPDATE] AlpacaProvider 컴포넌트 타입 갱신 - 컴포넌트 타입 갱신

--- src/atom/AlpacaProvider/AlpacaProvider.types.ts
+++ src/atom/AlpacaProvider/AlpacaProvider.types.ts
@@ -1,5 +1,5 @@
 /**
- * 알파카 Provider 컴포넌트 타입 모듈
+ * AlpacaProvider 컴포넌트 타입 모듈
  *
  * @author RWB
  * @since 2022.07.04 Mon 00:51:28
@@ -7,6 +7,6 @@
 
 import { ProviderProps } from 'react';
 
-import { ContextProps } from '../../common/context';
+import { Theme } from '../../common/context';
 
-export type AlpacaProviderProps = ProviderProps<ContextProps | undefined>
+export type AlpacaProviderProps = ProviderProps<Theme | undefined>
