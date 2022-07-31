f2d2624b8c5f45a589ea84e4c2f85bd97e9b6b0a
RWB0104 <psj2716@gmail.com>
2022-08-01 00:13:29 +0900

    :hammer: [UPDATE] Button 컴포넌트 갱신 - 컨텍스트 호출방식 변경 - 유니크 속성 추가

--- src/atom/Button/Button.tsx
+++ src/atom/Button/Button.tsx
@@ -1,5 +1,5 @@
 /**
- * 버튼 컴포넌트 모듈
+ * Button 컴포넌트 모듈
  *
  * @author RWB
  * @since 2022.06.06 Mon 17:45:00
@@ -12,10 +12,10 @@ import { RiLoader4Fill } from 'react-icons/ri';
 import styles from './Button.module.scss';
 import { ButtonProps } from './Button.types';
 
-import { Context } from '../../common/context';
+import { AlpacaContext } from '../../common/context';
 
 /**
- * 버튼 컴포넌트 JSX 반환 메서드
+ * Button 컴포넌트 JSX 반환 메서드
  *
  * @param {ButtonProps} param0: ButtonProps 인터페이스
  *
@@ -25,10 +25,10 @@ export default function Button({ border = 'flat', color = 'basic', loading, them
 {
 	const cn = classNames.bind(styles);
 
-	const ctx = useContext(Context);
+	const { theme: ctxTheme } = useContext(AlpacaContext);
 
 	return (
-		<button className={cn('button', theme || ctx?.theme || 'light', `border-${border}`, `color-${color}`, { loading }, className)} data-name='Button' {...props}>
+		<button className={cn('button', theme || ctxTheme || 'light', `border-${border}`, `color-${color}`, { loading }, className)} data-component='Button' {...props}>
 			<div className={cn('body')}>{children}</div>
 			<div className={cn('loading')}><RiLoader4Fill /></div>
 		</button>
