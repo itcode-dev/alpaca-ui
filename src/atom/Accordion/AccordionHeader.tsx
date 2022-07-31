c1823f27789f1acae5b6fac2631a6ad0d7128ca0
RWB0104 <psj2716@gmail.com>
2022-08-01 00:07:06 +0900

    :hammer: [FEATURE] AccordionHeader 컴포넌트 추가 - 컴포넌트 추가

---
+++ src/atom/Accordion/AccordionHeader.tsx
@@ -0,0 +1,44 @@
+/**
+ * AccordionHeader 컴포넌트 모듈
+ *
+ * @author RWB
+ * @since 2022.07.27 Wed 22:27:51
+ */
+
+import classNames from 'classnames/bind';
+import { useContext } from 'react';
+import { IoMdArrowDropdown } from 'react-icons/io';
+
+import styles from './Accordion.module.scss';
+import { AccordionHeaderProps } from './AccordionHeader.types';
+
+import { AccordionContext } from '../../common/context';
+
+/**
+ * AccordionHeader 컴포넌트 JSX 반환 메서드
+ *
+ * @param {AccordionHeaderProps} param0: AccordionHeaderProps
+ *
+ * @returns {JSX.Element} JSX
+ */
+export default function AccordionHeader({ direction, className, children, ...props }: AccordionHeaderProps): JSX.Element
+{
+	const { isOpen, setOpen } = useContext(AccordionContext);
+	const cn = classNames.bind(styles);
+
+	const svg = (
+		<div className={cn('accordion-svg')}>
+			<IoMdArrowDropdown />
+		</div>
+	);
+
+	return (
+		<button className={cn('accordion-header', className)} data-component='AccordionHeader' onClick={() => setOpen(!isOpen)} {...props}>
+			{direction === 'right' ? null : svg}
+
+			<div className={cn('accordion-title')}>{children}</div>
+
+			{direction === 'right' ? svg : null}
+		</button>
+	);
+}
