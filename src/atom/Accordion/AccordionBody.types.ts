4e118295bebdc79de86e33020e5b548b136a8b92
RWB0104 <psj2716@gmail.com>
2022-08-01 00:06:53 +0900

    :label: [FEATURE] AccordionBody 컴포넌트 타입 추가 - 컴포넌트 타입 추가

---
+++ src/atom/Accordion/AccordionBody.types.ts
@@ -0,0 +1,10 @@
+/**
+ * AccordionBody 컴포넌트 타입 모듈
+ *
+ * @author RWB
+ * @since 2022.07.29 Fri 02:08:55
+ */
+
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export type AccordionBodyProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
