073fbe7c44664a6bdbaedbd38f3b02cba61c41d0
RWB0104 <psj2716@gmail.com>
2022-08-01 00:13:52 +0900

    :label: [UPDATE] Button 컴포넌트 타입 갱신 - 색상 타입 추가

--- src/atom/Button/Button.types.ts
+++ src/atom/Button/Button.types.ts
@@ -1,5 +1,5 @@
 /**
- * 버튼 컴포넌트 타입 모듈
+ * Button 컴포넌트 타입 모듈
  *
  * @author RWB
  * @since 2022.07.11 Mon 15:03:27
@@ -10,7 +10,7 @@ import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 import { Theme } from '../../common/context';
 
 export type ButtonBorderType = 'flat' | 'round' | 'circle'
-export type ButtonColorType = 'basic' | 'primary' | 'submit' | 'warn' | 'error'
+export type ButtonColorType = 'basic' | 'primary' | 'submit' | 'warn' | 'error' | 'dark'
 
 export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 {
