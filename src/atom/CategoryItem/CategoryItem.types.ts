7955af32b07fd7444988c71119aeaa1f19673dc9
RWB0104 <psj2716@gmail.com>
2022-08-01 00:19:17 +0900

    :label: [UPDATE] CategoryItem 컴포넌트 타입 갱신 - 체크 속성 명칭 변경에 따른 반영

--- src/atom/CategoryItem/CategoryItem.types.ts
+++ src/atom/CategoryItem/CategoryItem.types.ts
@@ -1,5 +1,5 @@
 /**
- * 카테고리 아이템 컴포넌트 타입 모듈
+ * CategoryItem 컴포넌트 타입 모듈
  *
  * @author RWB
  * @since 2022.07.13 Wed 00:13:02
@@ -10,11 +10,11 @@ import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 export interface CategoryItemProps extends Omit<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, 'children'>
 {
 	category: string
+	checked?: boolean
 	name?: string
 	image?: string
 	count?: string | number
 	simplify?: boolean
-	isCheck?: boolean
 	refresh?: boolean
-	onSelect?: (isCheck: boolean) => void
+	onSelect?: (checked: boolean) => void
 }
