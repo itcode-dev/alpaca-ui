06ecf043404b22b1d6f9d7edd76bbd8549110ca9
박성진 <psj2716@gmail.com>
2022-08-02 09:43:07 +0900

    :label: [FEATURE] CollapseCardExtra 컴포넌트 타입 추가 - 컴포넌트 타입 추가

---
+++ src/atom/CollapseCard/CollapseCardExtra.types.ts
@@ -0,0 +1,10 @@
+/**
+ * CollapseCardExtra 컴포넌트 타입 모듈
+ *
+ * @author RWB
+ * @since 2022.08.01 Mon 13:41:31
+ */
+
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export type CollapseCardExtraProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
