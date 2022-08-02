744e45e4f6fedd09fc7c42ae41402a879c7e423b
박성진 <psj2716@gmail.com>
2022-08-02 09:42:28 +0900

    :hammer: [FEATURE] CollapseCardExtra 컴포넌트 추가 - 컴포넌트 추가

---
+++ src/atom/CollapseCard/CollapseCardExtra.tsx
@@ -0,0 +1,49 @@
+/**
+ * CollapseCardExtra 컴포넌트 모듈
+ *
+ * @author RWB
+ * @since 2022.08.01 Mon 13:21:11
+ */
+
+import classNames from 'classnames/bind';
+import { useContext, useEffect, useRef } from 'react';
+
+import styles from './CollapseCard.module.scss';
+import { CollapseCardExtraProps } from './CollapseCardExtra.types';
+
+import { CollapseCardContext } from '../../common/context';
+import { slide } from '../../common/util';
+
+/**
+ * CollapseCardExtra 컴포넌트 JSX 반환 메서드
+ *
+ * @param {CollapseCardExtraProps} param0: CollapseCardExtraProps 인터페이스
+ *
+ * @returns {JSX.Element} JSX
+ */
+export default function CollapseCardExtra({ className, children, ...props }: CollapseCardExtraProps): JSX.Element
+{
+	const cn = classNames.bind(styles);
+
+	const { value } = useContext(CollapseCardContext);
+
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() =>
+	{
+		const { current } = ref;
+
+		if (current)
+		{
+			slide(current, value?.isOpen);
+		}
+	}, [ value ]);
+
+	return value?.mode === 'extra' ? (
+		<div className={cn('collapse-card-extra')} data-component='CollapseCardExtra' ref={ref} {...props}>
+			<div className={cn('collapse-card-extra-wrapper', className)}>
+				{children}
+			</div>
+		</div>
+	) : null;
+}
