c51d98f9014380e7172428801ce0613a76ffec60
박성진 <psj2716@gmail.com>
2022-08-02 09:42:01 +0900

    :label: [FEATURE] CollapseCardBody 컴포넌트 타입 추가 - 컴포넌트 타입 추가

---
+++ src/atom/CollapseCard/CollapseCardBody.types.ts
@@ -0,0 +1,10 @@
+/**
+ * CollapseCardBody 컴포넌트 타입 모듈
+ *
+ * @author RWB
+ * @since 2022.08.01 Mon 13:12:11
+ */
+
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export type CollapseCardBodyProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
