2d3d29ab7be9210bd3aed300badb933e3dcc62f0
박성진 <psj2716@gmail.com>
2022-08-03 18:35:50 +0900

    :bulb: [UPDATE] ModalBody 컴포넌트 주석 갱신 - 주석 추가

--- src/atom/Modal/ModalBody.tsx
+++ src/atom/Modal/ModalBody.tsx
@@ -10,6 +10,13 @@ import classNames from 'classnames/bind';
 import styles from './Modal.module.scss';
 import { ModalBodyProps } from './ModalBody.types';
 
+/**
+ * ModalBody 컴포넌트 JSX 반환 메서드
+ *
+ * @param {ModalBodyProps} param0: ModalBodyProps 인터페이스
+ *
+ * @returns {JSX.Element} JSX
+ */
 export default function ModalBody({ className, ...props }: ModalBodyProps): JSX.Element
 {
 	const cn = classNames.bind(styles);
