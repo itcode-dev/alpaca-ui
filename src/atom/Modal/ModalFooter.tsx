c2e0d13cfdf335960ece6d709b404ef0f70783e5
박성진 <psj2716@gmail.com>
2022-08-03 18:36:41 +0900

    :hammer: [FEATURE] ModalFooter 컴포넌트 추가 - 컴포넌트 추가

---
+++ src/atom/Modal/ModalFooter.tsx
@@ -0,0 +1,26 @@
+/**
+ * ModalFooter 컴포넌트 모듈
+ *
+ * @author RWB
+ * @since 2022.08.03 Wed 09:58:03
+ */
+
+import classNames from 'classnames/bind';
+
+import styles from './Modal.module.scss';
+
+/**
+ * ModalFooter 컴포넌트 JSX 반환 메서드
+ *
+ * @param {ModalFooterProps} param0: ModalFooterProps 인터페이스
+ *
+ * @returns {JSX.Element} JSX
+ */
+export default function ModalFooter({ className, ...props }): JSX.Element
+{
+	const cn = classNames.bind(styles);
+
+	return (
+		<div className={cn('modal-footer', className)} data-component='ModalFooter' {...props} />
+	);
+}
