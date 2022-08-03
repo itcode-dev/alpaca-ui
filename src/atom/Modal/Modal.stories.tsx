2bc2044ce60b5ce6347025ed6068b8de10494721
박성진 <psj2716@gmail.com>
2022-08-03 18:36:21 +0900

    :lipstick: [UPDATE] Modal 컴포넌트 스타일 갱신 - 예제 변경

--- src/atom/Modal/Modal.stories.tsx
+++ src/atom/Modal/Modal.stories.tsx
@@ -7,11 +7,13 @@
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import classNames from 'classnames/bind';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ModalProps } from './Modal.types';
+import ModalFooter from './ModalFooter';
 
 import styles from '../../stories.module.scss';
+import Button from '../Button';
 
 import Modal from '.';
 
@@ -32,7 +34,7 @@ const cn = classNames.bind(styles);
 function getTemplate(args: ModalProps): ComponentStory<typeof Modal>
 {
 	return (
-		<section className={cn('root', args.theme)}>
+		<section className={cn('root', args.theme)} style={{ height: '100vh' }}>
 			<div className={cn('row')}>
 				<Modal {...args} />
 			</div>
@@ -49,7 +51,134 @@ Sandbox.args = {
 			</Modal.Header>
 
 			<Modal.Body>ModalBody</Modal.Body>
+
+			<ModalFooter>
+				<div style={{
+					display: 'flex',
+					gap: 10,
+					justifyContent: 'end'
+				}}
+				>
+					<Button border='round' color='submit'>확인</Button>
+					<Button border='round'>닫기</Button>
+				</div>
+			</ModalFooter>
 		</>
 	),
+	open: true,
 	theme: 'light'
 } as ModalProps;
+
+/**
+ * Dimmed 예시 JSX 반환 메서드
+ *
+ * @returns {JSX.Element} JSX
+ */
+export function Dimmed(): JSX.Element
+{
+	const [ isOpen, setOpen ] = useState(false);
+
+	return (
+		<section className={cn('root')} style={{ height: '100vh' }}>
+			<div className={cn('row')}>
+				<Button color='primary' onClick={() => setOpen(true)}>모달 호출 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<Modal open={isOpen} dimmed>
+				<Modal.Header>
+					<h2>ModalHeader</h2>
+				</Modal.Header>
+
+				<Modal.Body>ModalBody</Modal.Body>
+
+				<ModalFooter>
+					<div style={{
+						display: 'flex',
+						gap: 10,
+						justifyContent: 'end'
+					}}
+					>
+						<Button border='round' onClick={() => setOpen(false)}>닫기</Button>
+					</div>
+				</ModalFooter>
+			</Modal>
+		</section>
+	);
+}
+
+/**
+ * 강제 모달 예시 JSX 반환 메서드
+ *
+ * @returns {JSX.Element} JSX
+ */
+export function Forced(): JSX.Element
+{
+	const [ isOpen, setOpen ] = useState(false);
+
+	return (
+		<section className={cn('root')} style={{ height: '100vh' }}>
+			<div className={cn('row')}>
+				<Button color='primary' onClick={() => setOpen(true)}>모달 호출 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<div className={cn('row')}>
+				<Button>의미없는 버튼</Button>
+			</div>
+
+			<Modal open={isOpen} forced>
+				<Modal.Header>
+					<h2>ModalHeader</h2>
+				</Modal.Header>
+
+				<Modal.Body>ModalBody</Modal.Body>
+
+				<ModalFooter>
+					<div style={{
+						display: 'flex',
+						gap: 10,
+						justifyContent: 'end'
+					}}
+					>
+						<Button border='round' onClick={() => setOpen(false)}>닫기</Button>
+					</div>
+				</ModalFooter>
+			</Modal>
+		</section>
+	);
+}
