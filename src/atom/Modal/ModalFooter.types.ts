5de6e91df3f44eff7c3dc48fb7997ac477ec50fd
박성진 <psj2716@gmail.com>
2022-08-05 18:32:35 +0900

    :hammer: [FEATURE] 컴포넌트 추가 - 컴포넌트 추가

---
+++ src/atom/Modal/ModalFooter.types.ts
@@ -0,0 +1,10 @@
+/**
+ * ModalFooter 컴포넌트 타입 모듈
+ *
+ * @author RWB
+ * @since 2022.08.04 Thu 14:38:12
+ */
+
+import { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export type ModalFooterProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
---
+++ src/common/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 공통 모듈
+ *
+ * @author RWB
+ * @since 2022.08.04 Thu 14:57:32
+ */
+
+export * from '@alpaca/common/context';
+export * from '@alpaca/common/util';
