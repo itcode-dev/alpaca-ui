2640f4ef27cbe8bdb26f463c3dd5170b2b3475be
박성진 <psj2716@gmail.com>
2022-08-12 20:37:08 +0900

    :label: [FEATURE] ModalProvider 컴포넌트 타입 추가 - 컴포넌트 타입 추가

---
+++ src/atom/Modal/ModalProvider.types.ts
@@ -0,0 +1,10 @@
+/**
+ * ModalProvider 컴포넌트 타입 모듈
+ *
+ * @author RWB
+ * @since 2022.08.12 Fri 19:37:45
+ */
+
+import { ProviderProps } from 'react';
+
+export type ModalProviderProps = ProviderProps<boolean | undefined>
