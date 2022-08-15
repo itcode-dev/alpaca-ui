663e1547ff4b04f0a17b193f5b7ca7a30d3ee383
RWB0104 <psj2716@gmail.com>
2022-08-15 11:51:43 +0900

    :hammer: [FEATURE] InfiniteScroller 컴포넌트 추가 - 컴포넌트 추가

---
+++ src/atom/InfiniteScroller/index.ts
@@ -0,0 +1,10 @@
+/**
+ * InfiniteScroller 컴포넌트 인덱스 모듈
+ *
+ * @author RWB
+ * @since 2022.08.15 Mon 02:11:22
+ */
+
+import InfiniteScroller from './InfiniteScroller';
+
+export default InfiniteScroller;
