cdcb1deb679cba15f0740cf5590727aa16cc86c6
RWB0104 <psj2716@gmail.com>
2022-08-18 00:53:44 +0900

    :label: [FEATURE] SidebarProvider 컴포넌트 타입 추가 - 컴포넌트 타입 추가

---
+++ src/atom/Sidebar/SidebarProvider.types.ts
@@ -0,0 +1,10 @@
+/**
+ * SidebarProvider 컴포넌트 타입 모듈
+ *
+ * @author RWB
+ * @since 2022.08.15 Mon 18:16:31
+ */
+
+import { ProviderProps } from 'react';
+
+export type SidebarProviderProps = ProviderProps<boolean | undefined>
